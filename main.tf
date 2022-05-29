34ec4392d614a0ca9ebc70f098f8e32b0d0103ab
Nitish240 <105854082+Nitish240@users.noreply.github.com>
2022-05-29 22:43:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {                        #aws "laptop backup account key"
-  region     = "ap-northeast-3"
+  region     = "eu-west-2"
   access_key = "AKIA5RXZMCSEOJHECF24"
   secret_key = "5qvLnkPnv/H6gSSLALE03JPklOu/UGL4YCDu/Mpc"
 }
   resource "aws_ebs_volume" "example" {
-  availability_zone = "ap-northeast-3a"
+  availability_zone = "eu-west-2a"
   size              = 40
   tags = {
     Name = "web-server"
